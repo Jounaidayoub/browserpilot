ab2dbd3eb1c0044d95fed5ee357e45dd9e0e255f
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-10-12 23:29:22 +0100

    init

---
+++ src/sidepanel/App.tsx
@@ -0,0 +1,15 @@
+import crxLogo from '@/assets/crx.svg'
+import reactLogo from '@/assets/react.svg'
+import viteLogo from '@/assets/vite.svg'
+import HelloWorld from '@/components/HelloWorld'
+import './App.css'
+import { Button } from '@/components/ui/button'
+
+export default function App() {
+  return (
+    <>
+    
+    
+    </>
+  )
+}
