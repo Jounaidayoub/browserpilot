7ec0affa9ac06858969760d94b072653f8cf5e05
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-10-29 23:50:05 +0100

    add more themes

--- src/sidepanel/App.tsx
+++ src/sidepanel/App.tsx
@@ -1,16 +1,23 @@
-import crxLogo from '@/assets/crx.svg'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '@/assets/vite.svg'
-import HelloWorld from '@/components/HelloWorld'
-import './App.css'
-import { Button } from '@/components/ui/button'
-import ChatBotDemo from  "./ChatBotDemo"
+import crxLogo from "@/assets/crx.svg";
+import reactLogo from "@/assets/react.svg";
+import viteLogo from "@/assets/vite.svg";
+import HelloWorld from "@/components/HelloWorld";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider  } from "@/components/theme-provider.tsx";
+import { ThemeToggle } from "@/components/Toggle-theme.tsx";
+
+
+import ChatBotDemo from "./ChatBotDemo";
 export default function App() {
   return (
     <>
-
-    <ChatBotDemo />
-
+      <ThemeProvider defaultTheme="system" storageKey="tiny-ui-theme">
+        <div className="fixed top-4 right-4 z-50 flex gap-2">
+          <ThemeToggle />
+        </div>
+        <ChatBotDemo />
+      </ThemeProvider>
     </>
-  )
+  );
 }
