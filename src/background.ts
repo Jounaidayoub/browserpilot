5d277420092d0d3efa6b4633de4faf37b48c4a00
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-11-02 23:16:53 +0100

    testing with new tools

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,12 @@
-import { crx ,type CrxApplication} from "playwright-crx";
+import { crx, type CrxApplication } from "playwright-crx";
+import { captureDomSnapshot } from "./agent";
+import { parseEnv } from "util";
+
+// import { executeTool } from "./background/playwrightTools";
 (async () => {
   let crxApp: CrxApplication | null = null;
   try {
-    crxApp = await crx.start({ slowMo: 50 });
+    crxApp = await crx.start({ slowMo: 150 });
   } catch (e) {
     console.log("encounter an erro while instanting ", e);
   }
@@ -19,7 +23,7 @@ import { crx ,type CrxApplication} from "playwright-crx";
   //   for (const page of pages ?? []) {
   //     console.log("page url ",  page.url());
   //   }
-  
+
   //   console.log("background script started ");
   // } catch (e) {
   //   console.log("encounter an error while getting context ", e);
@@ -28,10 +32,9 @@ import { crx ,type CrxApplication} from "playwright-crx";
   // if (!crxApp) {
   //   throw new Error("Failed to get crx application");
   // }
-  // const 
+  // const
   // const BrowserContext = crxApp?.context();
   // (await BrowserContext.newPage()).locator("body
-  
 
   chrome.runtime.onStartup.addListener(async () => {
     console.log(`onStartup()`);
@@ -68,19 +71,35 @@ import { crx ,type CrxApplication} from "playwright-crx";
     console.log("got ", message, "from", sender);
     if (message?.action === "open-new-tab") {
       const url = message.url;
+      const Withcontent = message.Withcontent;
+      console.log("opening new tab for url ", url, " withcontent ", Withcontent);
       // const tabID=message.tabId;
 
       (async () => {
         try {
           // await newtab(url);
           console.log("New tab/page ");
-          
+
           // const page = await crxApp?.attach(236435725);
-          const page = await crxApp!.newPage({ url: url ,pinned:true});
+          let wait=null;
+          while (wait) {}
+          wait=true;
+          const page = await crxApp!.newPage({ url: url });
+          wait=null;
           console.log("we are going back ");
-          await page?.goBack();
+          let _snap = null;
+          if (Withcontent) {
+            _snap = await (page as any)?._snapshotForAI({
+              track: "response",
+            });
+          }
+          // crxApp?.pages()
+
+          // });
+          // crxApp;
+          // await page?.goBack();
           // page/
-          sendResponse({ success: true, tabId: null });
+          sendResponse({ success: true, tabId: null, content: _snap });
         } catch (error) {
           sendResponse({
             success: false,
@@ -100,6 +119,7 @@ import { crx ,type CrxApplication} from "playwright-crx";
         try {
           const page = await crxApp?.attach(tabID);
           const content = await page?.content();
+
           sendResponse({ success: true, content });
         } catch (error) {
           sendResponse({
@@ -111,6 +131,65 @@ import { crx ,type CrxApplication} from "playwright-crx";
 
       return true;
     }
+
+    if (message?.action === "get_page_dom_snapshot") {
+      console.log("getting dom snapshot... of ", message);
+      const tabID = message.tabId;
+      // const input = message.input;
+      (async () => {
+        try {
+          const page = await crxApp?.attach(tabID);
+          // const snapshot = await captureDomSnapshot(input, page!);
+
+          // console.log("snapshot captured ",snapshot, "with input ", input," tabID ", tabID);
+
+          // const _snap=await page?.locator("body").ariaSnapshot();
+
+          let _snap = await (page as any)?._snapshotForAI({
+            track: "response",
+          });
+          console.log("full snapshot: ", _snap);
+
+          // wait for 5 seconds
+          // console.log("waiting for 5 seconds...");
+          // await new Promise((resolve) => setTimeout(resolve, 5000));
+          // console.log("done waiting.");
+
+          // _snap=await (page as any)?._snapshotForAI({ track: 'response' });
+          // console.log("incremental snapshot: sss ", _snap);
+
+          sendResponse({ success: true, _snap });
+        } catch (error) {
+          console.log("error capturing snapshot: ", error);
+          sendResponse({
+            success: false,
+            error: error instanceof Error ? error.message : String(error),
+          });
+        }
+      })();
+
+      return true;
+    }
+
+    //   if (message?.action === "call-playwright-tool") {
+    //     const { toolName, arguments: args } = message;
+    //     (async () => {
+    //       try {
+    //         if (!crxApp) {
+    //           throw new Error("crxApp is not initialized");
+    //         }
+    //         const result = await executeTool(toolName, args || {}, crxApp) ;
+    //         sendResponse({ success: true, result });
+    //       } catch (error) {
+    //         sendResponse({
+    //           success: false,
+    //           error: error instanceof Error ? error.message : String(error),
+    //         });
+    //       }
+    //     })();
+
+    //     return true;
+    //   }
   });
 
   chrome.scripting.registerContentScripts([
