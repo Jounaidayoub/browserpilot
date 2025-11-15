34b035ec98b1baa7d603365078f05e5ffb50f25f
Ayoub <123956897+Jounaidayoub@users.noreply.github.com>
2025-11-15 09:45:41 +0100

    add typed input and the grouptabs by ids tool

--- src/tools/Tabs.ts
+++ src/tools/Tabs.ts
@@ -1,9 +1,11 @@
 import z, { ZodObject } from "zod";
-import { Tool } from "./types";
+import { emptyInput, Tool } from "./types";
+import { get } from "http";
 
-const get_groups: Tool<null> = {
+const get_groups: Tool<typeof emptyInput> = {
   name: "get_groups",
   description: "Get all tab groups in the browser.",
+  inputSchema: emptyInput,
   execute: async () => {
     const Groups = await chrome.tabGroups.query({});
 
@@ -11,9 +13,10 @@ const get_groups: Tool<null> = {
   },
 };
 
-const get_tabs: Tool<null> = {
+const get_tabs: Tool<typeof emptyInput> = {
   name: "get_tabs",
   description: "Get all tabs in the browser.",
+  inputSchema: emptyInput,
   execute: async () => {
     const tabs = await chrome.tabs.query({});
 
@@ -31,4 +34,68 @@ const get_tabs: Tool<null> = {
   },
 };
 
-export { get_groups, get_tabs };
+const close_tabsType = z.object({
+  tabIds: z.array(z.number()),
+});
+const close_tabs: Tool<typeof close_tabsType> = {
+  name: "close_tabs",
+  description: "Close tabs by their IDs.",
+  inputSchema: close_tabsType,
+  execute: async ({ tabIds }) => {
+    chrome.tabs.query({}, () => {
+      chrome.tabs.remove(tabIds);
+    });
+
+    return "Tabs closed with IDs: " + tabIds.join(", ");
+  },
+};
+
+const group_tabs_by_idsInput = z.object({
+  groups: z.array(
+    z.object({
+      tabIds: z.array(z.number()),
+      color: z
+        .enum([
+          "blue",
+          "cyan",
+          "green",
+          "grey",
+          "orange",
+          "pink",
+          "purple",
+          "red",
+          "yellow",
+        ])
+        .optional(),
+      title: z.string(),
+    })
+  ),
+});
+
+const group_tabs_by_ids: Tool<typeof group_tabs_by_idsInput> = {
+  name: "group_tabs_by_ids",
+  description:
+    "Group the given tabs by their ids into new groups. Each group should include tabIds, color, and title. Organizes tabs into topics based on URLs and titles. Example: { groups: [{ tabIds: [123, 456], color: 'blue', title: 'Docs' }] }",
+  inputSchema: group_tabs_by_idsInput,
+  execute: async ({ groups }) => {
+    groups.forEach(async (group) => {
+      console.log("grouping", group);
+      const { tabIds, title, color } = group;
+
+      const groupid = await chrome.tabs.group({
+        tabIds: tabIds as [number, ...number[]],
+      });
+      console.log("created group", groupid);
+      await chrome.tabGroups.update(groupid, {
+        title: title,
+        color: color,
+      });
+      console.log(
+        `Grouped tabs ${tabIds} into group ${groupid} with title "${title}" and color "${color}"`
+      );
+    });
+    return "Grouped tabs into " + groups.length + " groups.";
+  },
+};
+
+export { get_groups, get_tabs, close_tabs, group_tabs_by_ids };
